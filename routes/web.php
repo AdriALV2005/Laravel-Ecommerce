d57739c30e0f78400c9a5d0887bf7c816998f926
AdriALV2005 <adrimon7221@gmail.com>
2024-12-08 15:04:01 -0500

    Agregando brands

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::middleware(['auth'])->group(function () {
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
 });
-Route::middleware(['auth',AuthAdmin::class])->group(function () {
+Route::middleware(['auth', AuthAdmin::class])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
 });
